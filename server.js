6b56ce37a1969b6c2800e6436419fe14b6a6b10b
Alessandro Ruggieri <alex@iMac-di-Alessandro.local>
2020-04-24 11:33:46 +0200

    prod

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.static(__dirname + '/dist'));
 
 configureAPI(app);
 
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+
 app.listen(PORT,()=>{
     console.log("Server listening on port:"+PORT)
 })
