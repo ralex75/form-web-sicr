87207607e51538bd4c7c58cdb8d8a1437d9db5d5
alex <alex>
2020-07-20 12:58:42 +0200

    cambiato url in service

--- src/js/services.js
+++ src/js/services.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-//axios.defaults.baseURL = 'http://webapp-dev.roma1.infn.it';
-axios.defaults.baseURL = 'http://localhost:3000/';
+axios.defaults.baseURL = 'http://webapp-dev.roma1.infn.it';
+//axios.defaults.baseURL = 'http://localhost:3000/';
 
 function checkUserAuth(fail=null){
    
