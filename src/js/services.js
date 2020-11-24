0e03e7340fb94954ad575637b6800d1504e1fb89
alex <alex>
2020-11-24 15:50:12 +0100

    fix

--- src/js/services.js
+++ src/js/services.js
@@ -3,7 +3,7 @@ import "regenerator-runtime/runtime.js"; //per webpack funzioni async
 
 
 //axios.defaults.baseURL = 'http://www.roma1.infn.it/conference/wwwsicr/supporto/';
-axios.defaults.baseURL = '/sicr/supporto/';
+//axios.defaults.baseURL = '/sicr/supporto/';
 
 
 const user={
