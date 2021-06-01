25448810785b42a02098eb81a67976501e1667ef
Alex <alessandro.ruggieri@roma1.infn.it>
2021-06-01 15:51:40 +0200

    app.js User.isValid logica controllo spostata nel server

--- server/api/auth.js
+++ server/api/auth.js
@@ -41,7 +41,7 @@ function authToken(req,res,next){
     testUUID='781f6f6d-d090-4d67-b74d-63d079324bd4'
     
     //testUUID='b9abec6d-4ab1-4011-b344-682433ccead1'
-  //testUUID="9228ab75-d9b0-4573-93af-cfd1a6f44848"
+    //testUUID="9228ab75-d9b0-4573-93af-cfd1a6f44848"
     
     
     var uid=req.headers["x-uuid"] || testUUID;
