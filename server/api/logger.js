c3a3c7d6b299f4533c06a91ce4be76c42a4d949f
Alessandro Ruggieri <alessandro.ruggieri@roma1.infn.it>
2021-10-25 10:36:40 +0200

    fix view support.js: aggiunto utente della segnalazione

--- server/api/logger.js
+++ server/api/logger.js
@@ -3,10 +3,11 @@ const moment = require('moment')
 const express = require('express');
 const router = express.Router();
 const {sendMail} =require("./mailer")
-const {getUser} = require('./user') 
+const {getUser} = require('./user'); 
+const { async } = require("regenerator-runtime");
 
 
-const dump=(fileName,data)=>{
+/*const dump=(fileName,data)=>{
 
     let path=__dirname+`/../logs/${fileName}.txt`
     let content="==================================\n"
@@ -16,7 +17,7 @@ const dump=(fileName,data)=>{
     
     fs.appendFileSync(path,content)
 
-}
+}*/
 
 router.post("/", async (req,res)=>{
     let user=req.body.user;
@@ -24,30 +25,17 @@ router.post("/", async (req,res)=>{
     user= await getUser(user.uuid)
 
     if(!user) return  res.status(500).json({"err":`No user found`})
+    let status = user.isAuthorized ? "ok" : "bad"
+    let subject=`form-web - ${status} user status: ${user.name} ${user.surname}`
+    let from='supporto@roma1.infn.it'
+    let to=from;
+   
+    const {templates}=require("./templates")
 
-    if(user.isAuthorized) return res.json({"txt":`user is ok`})
-
-    const {templates}=require("./templates");
-    
-
-    let txt=`
-    
-        <pre>
-        ${templates.userInfo(user)}
-        -----------------------------------------------------------------
-        Autorizzato     : ${user.isAuthorized ? 'SI': 'NO'}    
-        -----------------------------------------------------------------
-        Ruolo roma1     : ${user.role ? 'SI' : 'NO'}
-        LOA2            : ${user.loa2 ? 'SI' : 'NO'}
-        Disciplinare    : ${user.policies ? 'SI' : 'NO'}
-        Corso sicurezza : ${user.itsec ? 'SI' : 'NO'}
-        GraceTime       : ${user.gracetime ? 'SI' : 'NO'}
-        </pre>
-        
-    `
-        
-    sendMail('supporto@roma1.infn.it','supporto@roma1.infn.it',	`form-web - user: ${user.name} ${user.surname} - bad status`,txt).then(res=>{
-        console.log(`${user.name} ${user.surname} missing login requirements`)
+    let txt=`<pre>${templates.completeUserInfo(user)}</pre>`
+     
+    sendMail(from,to,subject,txt).then(res=>{
+        console.log(`${subject}`)
     }).catch(err=>{
         console.log(err);
     }).finally(_=>{
