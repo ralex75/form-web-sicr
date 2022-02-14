d17909f071744da704e632b9f899aaf20792d781
Alessandro Ruggieri <alessandro.ruggieri@roma1.infn.it>
2022-02-14 10:30:37 +0100

    fix userdb.js modificato search query

--- server/api/userdb.js
+++ server/api/userdb.js
@@ -23,15 +23,19 @@ const getUsers=async function(keywords)
     let _keywords=keywords.replace(/\'/g,"''")
     let AND=""
 
-    _keywords.split(" ").forEach(k=>{
+    /*_keywords.split(" ").forEach(k=>{
         AND+=` AND (x.email ilike '${k}' OR x.cdf ilike '${k}%' OR x.tel1 ilike '${k}%' OR x.nome ilike '${k}%' OR x.cognome ilike '${k}%')`
-    })
+    })*/
+
 
     var statoOSP = "'%'"
     var statoFIX = " like '%'"
 
-    var queryOsp=`SELECT nome as \"name\", cognome as \"surname\", cdf as \"cf\", email, tel1 as \"phone\",'OSPITE' as \"role\", inizio_visita, fine_visita, tipo, gruppo, stato, referente,responsabile FROM (${querydip["ospiti"]}) x WHERE x.stato like ${statoOSP} ${AND} order by x.cognome`;
-    var queryFix=`SELECT nome as \"name\", cognome as \"surname\", cdf as \"cf\", email, tel1 as \"phone\",'FISSO' as \"role\", qualifica, ente,expire_date, stato FROM (${querydip["fissi"]}) x WHERE x.stato ${statoFIX} ${AND} order by x.cognome`;
+    //var queryOsp=`SELECT nome as \"name\", cognome as \"surname\", cdf as \"cf\", email, tel1 as \"phone\",'OSPITE' as \"role\", inizio_visita, fine_visita, tipo, gruppo, stato, referente,responsabile FROM (${querydip["ospiti"]}) x WHERE x.stato like ${statoOSP} ${AND} order by x.cognome`;
+    //var queryFix=`SELECT nome as \"name\", cognome as \"surname\", cdf as \"cf\", email, tel1 as \"phone\",'FISSO' as \"role\", qualifica, ente,expire_date, stato FROM (${querydip["fissi"]}) x WHERE x.stato ${statoFIX} ${AND} order by x.cognome`;
+    
+    let queryOsp=`SELECT nome as \"name\", cognome as \"surname\", cdf as \"cf\", email, tel1 as \"phone\",'OSPITE' as \"role\", inizio_visita, fine_visita, tipo, gruppo, stato, referente,responsabile FROM (${querydip["ospiti"]}) x WHERE (nome || cognome || cdf || email || tel1) ilike '%${_keywords}%' order by x.cognome`;
+    let queryFix=`SELECT nome as \"name\", cognome as \"surname\", cdf as \"cf\", email, tel1 as \"phone\",'FISSO' as \"role\", qualifica, ente,expire_date, stato FROM (${querydip["fissi"]}) x WHERE (nome || cognome || cdf || email || tel1) ilike '%${_keywords}%' order by x.cognome`;
     
     var users_osp_db=await getUserDBAll(queryOsp);
     var users_fix_db=await getUserDBAll(queryFix);
